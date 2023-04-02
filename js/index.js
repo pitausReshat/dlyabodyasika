6e187090d23847b8b81e20728d914d97b3a2809e
pitausReshat <dg272313@gmail.com>
2023-04-02 18:35:02 +0300

    Changes

--- js/index.js
+++ js/index.js
@@ -12,19 +12,26 @@
 
 
 
-   function addList(){
+
+
+
+  function addList(){
     event.preventDefault();
-    let txtVal = document.getElementById('txtVal').value, 
-      listNode = document.getElementById('list'), 
+    let txtVal = document.getElementById('txtVal').value.trim(); 
+    if (txtVal === '') {
+      alert('Please enter something in the input field.');
+      return;
+    }
+  
+    let listNode = document.getElementById('list'), 
       liNode = document.createElement('LI'), 
       txtNode = document.createTextNode(txtVal);
-
-          liNode.appendChild(txtNode);
-    
-          listNode.appendChild(liNode);
-          document.getElementById('txtVal').value = '';
-
+  
+    liNode.appendChild(txtNode);
+    listNode.appendChild(liNode);
+    document.getElementById('txtVal').value = '';
   };
+  
 
 
  
